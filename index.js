fe3a044dd0a7389995673a2da08ece8fc3da227c
afrontend <autoscripts@gmail.com>
2021-02-20 12:55:21 +0900

    Use machine ip address in fastify

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ fastify.register(require('fastify-static'), {
 })
 
 // Run the server!
-fastify.listen(3000, function (err, address) {
+fastify.listen(3000, '0.0.0.0', function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
