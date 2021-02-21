cff1c7395abf38324cc0cb876247eb0ce8c00354
afrontend <autoscripts@gmail.com>
2021-02-21 11:58:43 +0900

    Decrease the sensitivity of volume control

--- public/js/main.js
+++ public/js/main.js
@@ -17,8 +17,8 @@ var scrollUpCount = 0;
 var scrollDownCount = 0;
 var volumeUpCount = 0;
 var volumeDownCount = 0;
-var LIMIT_SCROLL_COUNT = 1
-var LIMIT_VOLUME_COUNT = 5
+var LIMIT_SCROLL_START = 1
+var LIMIT_VOLUME_START = 20 
 
 // listen to events...
 mc.on("panleft panright panup pandown tap press", function(ev) {
@@ -26,7 +26,7 @@ mc.on("panleft panright panup pandown tap press", function(ev) {
     console.log(panel.textContent)
     if (ev.type === 'panup') {
       scrollUpCount = scrollUpCount + 1
-      if (scrollUpCount > LIMIT_SCROLL_COUNT) {
+      if (scrollUpCount > LIMIT_SCROLL_START) {
         send('/scrollUp')
         // scrollUpCount = 0;
       }
@@ -34,7 +34,7 @@ mc.on("panleft panright panup pandown tap press", function(ev) {
       volumeDownCount = 0;
     } else if (ev.type === 'pandown') {
       scrollDownCount = scrollDownCount + 1
-      if (scrollDownCount > LIMIT_SCROLL_COUNT) {
+      if (scrollDownCount > LIMIT_SCROLL_START) {
         send('/scrollDown')
         // scrollDownCount = 0;
       }
@@ -42,17 +42,17 @@ mc.on("panleft panright panup pandown tap press", function(ev) {
       volumeDownCount = 0;
     } else if (ev.type === 'panleft') {
       volumeDownCount = volumeDownCount + 1
-      if (volumeDownCount > LIMIT_VOLUME_COUNT) {
+      if (volumeDownCount > LIMIT_VOLUME_START) {
         send('/volumeDown')
-        volumeDownCount = 0
+        volumeDownCount = LIMIT_VOLUME_START - 15
       }
       scrollUpCount = 0;
       scrollDownCount = 0;
     } else if (ev.type === 'panright') {
       volumeUpCount = volumeUpCount + 1
-      if (volumeUpCount > LIMIT_VOLUME_COUNT) {
+      if (volumeUpCount > LIMIT_VOLUME_START) {
         send('/volumeUp')
-        volumeUpCount = 0
+        volumeUpCount = LIMIT_VOLUME_START - 15
       }
       scrollUpCount = 0;
       scrollDownCount = 0;
