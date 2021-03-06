5d76b8c00903f8d05d7ff5c715ea5c6d026f743c
afrontend <autoscripts@gmail.com>
2021-03-06 15:42:51 +0900

    Use buttons to control volume

--- public/js/main.js
+++ public/js/main.js
@@ -1,65 +1,52 @@
-var panel = document.getElementById('panel');
+var panel = document.getElementById('panel')
+var leftButton = document.querySelector('.left')
 
 var send = function (url) {
-  let request = new XMLHttpRequest();
-  request.open("POST", url);
+  let request = new XMLHttpRequest()
+  request.open("POST", url)
   request.onload = function(){
-    console.log(request.response);
+    console.log(request.response)
   }
-  request.send();
+  request.send()
 }
 
-var mc = new Hammer(panel);
+leftButton.addEventListener('click', () => {
+  window.console.log('down')
+  send('/volumeDown')
+})
 
-mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+var rightButton = document.querySelector('.right')
+rightButton.addEventListener('click', () => {
+  window.console.log('up')
+  send('/volumeUp')
+})
 
-var scrollUpCount = 0;
-var scrollDownCount = 0;
-var volumeUpCount = 0;
-var volumeDownCount = 0;
+var mc = new Hammer(panel)
+
+mc.get('pan').set({ direction: Hammer.DIRECTION_ALL })
+
+var scrollUpCount = 0
+var scrollDownCount = 0
 var LIMIT_SCROLL_START = 1
-var LIMIT_VOLUME_START = 20
 
 // listen to events...
-mc.on("panleft panright panup pandown tap press", function(ev) {
-    panel.textContent = ev.type +" gesture detected.";
+mc.on("panup pandown tap press", function(ev) {
+    panel.textContent = ev.type +" gesture detected."
     console.log(panel.textContent)
     if (ev.type === 'panup') {
       scrollUpCount = scrollUpCount + 1
       if (scrollUpCount > LIMIT_SCROLL_START) {
         send('/scrollUp')
-        // scrollUpCount = 0;
+        // scrollUpCount = 0
       }
-      volumeUpCount = 0;
-      volumeDownCount = 0;
     } else if (ev.type === 'pandown') {
       scrollDownCount = scrollDownCount + 1
       if (scrollDownCount > LIMIT_SCROLL_START) {
         send('/scrollDown')
-        // scrollDownCount = 0;
+        // scrollDownCount = 0
       }
-      volumeUpCount = 0;
-      volumeDownCount = 0;
-    } else if (ev.type === 'panleft') {
-      volumeDownCount = volumeDownCount + 1
-      if (volumeDownCount > LIMIT_VOLUME_START) {
-        send('/volumeDown')
-        volumeDownCount = LIMIT_VOLUME_START - 15
-      }
-      scrollUpCount = 0;
-      scrollDownCount = 0;
-    } else if (ev.type === 'panright') {
-      volumeUpCount = volumeUpCount + 1
-      if (volumeUpCount > LIMIT_VOLUME_START) {
-        send('/volumeUp')
-        volumeUpCount = LIMIT_VOLUME_START - 15
-      }
-      scrollUpCount = 0;
-      scrollDownCount = 0;
     } else {
-      scrollUpCount = 0;
-      scrollDownCount = 0;
-      volumeUpCount = 0;
-      volumeDownCount = 0;
+      scrollUpCount = 0
+      scrollDownCount = 0
     }
-});
+})
