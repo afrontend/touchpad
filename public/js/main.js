f8af8fb45ba2b3b252e754ff07582a3f84427655
afrontend <autoscripts@gmail.com>
2021-03-07 13:05:06 +0900

    Typo

--- public/js/main.js
+++ public/js/main.js
@@ -29,22 +29,22 @@ var LIMIT_SCROLL_START = 1
 
 // listen to events...
 mc.on("panup pandown tap press", function(ev) {
-    panel.textContent = ev.type +" gesture detected."
-    console.log(panel.textContent)
-    if (ev.type === 'panup') {
-      scrollUpCount = scrollUpCount + 1
-      if (scrollUpCount > LIMIT_SCROLL_START) {
-        send('/scrollUp')
-        // scrollUpCount = 0
-      }
-    } else if (ev.type === 'pandown') {
-      scrollDownCount = scrollDownCount + 1
-      if (scrollDownCount > LIMIT_SCROLL_START) {
-        send('/scrollDown')
-        // scrollDownCount = 0
-      }
-    } else {
-      scrollUpCount = 0
-      scrollDownCount = 0
+  panel.textContent = ev.type +" gesture detected."
+  console.log(panel.textContent)
+  if (ev.type === 'panup') {
+    scrollUpCount = scrollUpCount + 1
+    if (scrollUpCount > LIMIT_SCROLL_START) {
+      send('/scrollUp')
+      // scrollUpCount = 0
     }
+  } else if (ev.type === 'pandown') {
+    scrollDownCount = scrollDownCount + 1
+    if (scrollDownCount > LIMIT_SCROLL_START) {
+      send('/scrollDown')
+      // scrollDownCount = 0
+    }
+  } else {
+    scrollUpCount = 0
+    scrollDownCount = 0
+  }
 })
